85f6f53cf37e671c453ac4deca79ddb92b3d6179
Havindu21 <havindufonseka@gmail.com>
2024-02-09 02:16:12 +0530

    initial homepage-navbar

--- meo-wa-project/src/pages/HomePage.jsx
+++ meo-wa-project/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import HomeContainer from '../components/HomeContainer';
+import Grid from '@mui/material/Unstable_Grid2';
+import Sidebar from '../components/Sidebar';
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <Navbar />
+      <Grid container >
+        <Grid xs={1}><Sidebar /></Grid>
+        <Grid xs={11}><HomeContainer /></Grid>
+      </Grid>
+    </div>
   )
 }
 
